517a6f9283f569007af5b69538411f56b048c936
Josh Waterson <jjwaterson@hotmail.co.uk>
2022-08-29 14:16:26 +0100

    moved ThreadReference to its own file

--- src/main/java/instrumentation/ThreadMarker.java
+++ src/main/java/instrumentation/ThreadMarker.java
@@ -19,21 +19,3 @@ public class ThreadMarker {
         return new ThreadReference(this, thread);
     }
 }
-
-class ThreadReference {
-    String[] elements;
-    ThreadReference(ThreadMarker tm, Thread th) {
-        elements = new String[]{
-                String.valueOf(tm.elements[0]),
-                String.valueOf(tm.elements[1]),
-                String.valueOf(tm.elements[2]),
-                String.valueOf(th),
-                String.valueOf(th.hashCode())
-        };
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(this.elements);
-    }
-}
